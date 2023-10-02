b6d0a10faa0d76ce633b1830c95a3b714defd147
Kevin <alaqdd0303@gmail.com>
2023-10-02 17:21:36 +0800

    commnet

--- Data Structure/ConsoleApplication1/ConsoleApplication1.cpp
+++ Data Structure/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,20 +1,46 @@
-﻿// ConsoleApplication1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include<stdio.h>
+#include<stdlib.h>
 
-#include <iostream>
+typedef int SLLDataType;		//single acyclic linked list, SL for short
 
-int main()
+typedef struct SLLNode
 {
-    std::cout << "Hello World!\n";
+	SLLDataType data;		//int data
+	struct SLLNode* next;
+};
+
+void SLLPrint(SLLNode* phead)		//head pointer, phead for short
+{
+	SLLNode* p = phead;
+	while (p != NULL)
+	{
+		printf("%d->", p->data);
+		p = p->next;
+	}
+	printf("NULL");
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
+SLLNode* BuySLLNode(SLLDataType x)
+{
+	SLLNode* newnode = (SLLNode*)malloc(sizeof(SLLNode));
+	if (newnode == NULL)
+	{
+		perror("malloc");
+		exit(-1);
+	}
+	newnode->data = x;
+	newnode->next = NULL;;
+	return newnode;
+}
 
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+void SLLPushBack(SLLNode** phead, SLLDataType x)	//尾插
+{
+	if (*phead == NULL) *phead = BuySLLNode(x);
+	else 
+	{
+		while(*phead->data)
+		{
+			*phead = *phead->next;
+		}
+	}
+}
